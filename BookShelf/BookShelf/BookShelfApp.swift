7f2ff5499e67cba74081831b37a4ae016e70403d
KseniaDidusenko <ksenia.didusenko@gmail.com>
2023-11-15 13:29:36 +0200

    Initial commit

---
+++ BookShelf/BookShelf/BookShelfApp.swift
@@ -0,0 +1,17 @@
+//
+//  BookShelfApp.swift
+//  BookShelf
+//
+//  Created by Ksenia on 15.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct BookShelfApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
